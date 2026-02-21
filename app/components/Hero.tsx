4f196e1b44a2ffd40bf00e982247a605b0dd5d16
Emre Gundogdu <emreguendogdu@yandex.com>
2026-02-21 03:52:48 +0300

    switch to cover

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -120,7 +120,7 @@ export function Hero() {
           src="/images/hero-photo-collage.png"
           alt="Happy patients"
           fill
-          className="object-contain"
+          className="object-cover"
           priority
         />
       </div>
