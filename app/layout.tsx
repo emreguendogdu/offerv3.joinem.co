5d797be85527045d045cef6b52d4c9f560da42e6
Emre Gundogdu <emreguendogdu@yandex.com>
2026-03-06 23:21:25 +0700

    feat: Add EverFlow scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,6 +68,27 @@ export default function RootLayout({
           })(window,document,'script','dataLayer','GTM-TXLL9VGZ'));
         `}</Script>
 
+        {/* Everflow */}
+        <Script
+          id="everflow-main"
+          strategy="beforeInteractive"
+          src="https://www.mnbvpo8trk.com/scripts/main.js"
+        />
+        <Script id="everflow-click" strategy="beforeInteractive">{`
+          EF.click({
+            offer_id: EF.urlParameter('oid'),
+            affiliate_id: EF.urlParameter('affid'),
+            source_id: EF.urlParameter('source_id'),
+            sub1: EF.urlParameter('sub1'),
+            sub2: EF.urlParameter('sub2'),
+            sub3: EF.urlParameter('sub3'),
+            sub4: EF.urlParameter('sub4'),
+            sub5: EF.urlParameter('sub5'),
+            uid: EF.urlParameter('uid'),
+            transaction_id: EF.urlParameter('_ef_transaction_id'),
+          });
+        `}</Script>
+
         {/* Katalys */}
         <Script
           id="katalys"
